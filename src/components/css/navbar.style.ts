27796486b4d2b7a0fe19226f96b5cfd4631fadaf
luckmer <woszczo04@gmail.com>
2021-11-19 11:39:01 +0100

    main css folder for components

---
+++ src/components/css/navbar.style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: auto;
+  padding: 20px;
+  width: 100%;
+  padding: 20px 40px 20px 40px;
+  background-color: #0868cf;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  color: white;
+  font-family: "Shippori Antique", sans-serif;
+  text-transform: uppercase;
+`;
+
+export const H1 = styled.h1`
+  cursor: pointer;
+`;
