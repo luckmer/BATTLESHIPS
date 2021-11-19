8e6720d4dfecd0ba57aa3c7e6e92d09051dd60a3
luckmer <woszczo04@gmail.com>
2021-11-19 11:37:57 +0100

    created basic design

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { Nav, Section, H1 } from "../css/navbar.style";
 
-function NavBar() {
-  return <nav></nav>;
-}
+const NavBar = () => {
+  return (
+    <Nav>
+      <Section>
+        <div>
+          <H1>BATTLESHIP</H1>
+        </div>
+        <div>
+          <H1>
+            <AiOutlineHome />
+          </H1>
+        </div>
+      </Section>
+    </Nav>
+  );
+};
 
 export default NavBar;
