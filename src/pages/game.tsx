ce2cc20f6f3152e1bcfb381709cc2fd85d2f1bf9
luckmer <woszczo04@gmail.com>
2021-11-19 13:12:57 +0100

    added ships

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,11 +1,21 @@
-import styled from "styled-components";
 import GenerateBoard from "../service/boardCreator/board";
+import ShipPanel from "../service/ships/shipPanel";
+import { boardsInterface } from "./interface";
+import {
+  Section,
+  Rotate,
+  Div,
+  Grid,
+  Footer,
+  ShipContainer,
+  ShipGrid,
+  Ship
+} from "../css/game.style";
 
 const Game = () => {
   const player = GenerateBoard("player");
   const enemy = GenerateBoard("enemy");
-
-  const btns = ["start game", "rotate"];
+  const { shipData } = ShipPanel();
 
   return (
     <Section>
@@ -22,52 +32,17 @@ const Game = () => {
           ))}
         </Div>
       </Rotate>
-      <ButtonPanel>
-        {btns.map((el: string) => (
-          <div key={el}>
-            <button name={el}>{el}</button>
-          </div>
-        ))}
-      </ButtonPanel>
-      <Footer>boats!</Footer>
+      <Footer>
+        <ShipContainer>
+          {shipData.map(({ id, size }: boardsInterface) => (
+            <ShipGrid key={id}>
+              <Ship size={size} />
+            </ShipGrid>
+          ))}
+        </ShipContainer>
+      </Footer>
     </Section>
   );
 };
 
 export default Game;
-
-const Div = styled.div`
-  margin: 2vmin;
-  display: grid;
-  background-color: hsl(200, 100%, 50%);
-  grid-template-rows: repeat(10, 4.6vmin);
-  grid-template-columns: repeat(10, 4.6vmin);
-`;
-
-const Grid = styled.div`
-  border: 1px solid hsla(0, 0%, 100%, 0.2);
-  transition: all 0.2s;
-  &:hover {
-    background-color: #0868cf;
-  }
-`;
-
-const Rotate = styled.div`
-  display: flex;
-`;
-
-const ButtonPanel = styled.div`
-  margin: 2vmin;
-`;
-
-const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  max-width: 1800px;
-  margin: 0 auto;
-`;
-
-const Footer = styled.footer`
-  margin: 2vmin;
-`;
