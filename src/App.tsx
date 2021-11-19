d8cf9f59fc34437434b43d97146093201a62730f
luckmer <woszczo04@gmail.com>
2021-11-19 11:42:36 +0100

    basic components update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
-}
+import GenerateBoard from "./service/boardCreator/board";
+import NavBar from "./components/navbar";
+
+const App = () => {
+  const player = GenerateBoard("player");
+
+  console.log(player);
+
+  return (
+    <main>
+      <NavBar />
+      <div></div>
+    </main>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./css/Global.style";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById("root")
 );
