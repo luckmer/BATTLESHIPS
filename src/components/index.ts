b9eee290db5ffbc0e336d70911b5a5f8bdf47f67
luckmer <woszczo04@gmail.com>
2021-12-01 14:18:49 +0100

    main import /export

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as NoteInformation } from "./notifications/NoteInformation";
+export { default as PlayerBoard } from "./PlayerBoard/PlayerBoard";
+export { default as ButtonPanel } from "./buttonPanel/ButtonPanel";
+export { default as GameOverPanel } from "./gameOver/GameOver";
+export { default as SideMenu } from "./sideMenu/SideMenu";
+export { default as NavBar } from "./navbar/index";
+export { default as Ships } from "./Ships/Ships";
