5a32463df6f370da491a929291848997ea7ef8dd
luckmer <woszczo04@gmail.com>
2021-12-09 15:20:06 +0100

    created help pages

---
+++ src/css/help.style.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  max-width: 700px;
+  width: 100%;
+  margin: 40px auto;
+  padding: 0 10px;
+`;
+export const Header = styled.header`
+  background-color: #9ecdff;
+  width: 100%;
+  border-radius: 5px;
+  text-align: center;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+  font-family: "Playfair Display", serif;
+`;
+export const H1 = styled.h1`
+  color: #3a5b86;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+export const Small = styled.small`
+  color: #3a5b86;
+  font-weight: bold;
+  padding-bottom: 15px;
+  padding-top: 15px;
+`;
+export const HeaderPanel = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+`;
+export const PCContainer = styled.div`
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  border-radius: 5px;
+  margin-top: 20px;
+  padding: 10px 20px 10px 20px;
+  margin-bottom: 50px;
+`;
+export const PcDescriptionPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const P = styled.p`
+  color: #3a5b86;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: 18px;
+`;
--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,5 +1,75 @@
+import {
+  Section,
+  Header,
+  HeaderPanel,
+  Small,
+  H1,
+  PCContainer,
+  PcDescriptionPanel,
+  P
+} from "../css/help.style";
+
 const Help = () => {
-  return <div></div>;
+  return (
+    <Section>
+      <Header>
+        <HeaderPanel>
+          <Small> Need Help? </Small>
+        </HeaderPanel>
+        <Small> 24/7 </Small>
+        <H1>Help Panel</H1>
+      </Header>
+      <PCContainer>
+        <div>
+          <H1>Pc Controll</H1>
+        </div>
+        <PcDescriptionPanel>
+          <Small>player ships</Small>
+          <P>
+            The boat control panel is located in the bottom left corner, after
+            opening it drag the ship to the player's area, then drop the ship by
+            releasing the mouse button.
+          </P>
+          <Small>player history</Small>
+          <P>
+            the move history is located in the bottom left corner , when opened
+            you can select the previous move , at the bottom of the menu there
+            will be a button to stop updating the selected move history
+          </P>
+          <Small>player Attack</Small>
+          <P>to make an attack on an enemy, click on the enemy's board</P>
+        </PcDescriptionPanel>
+      </PCContainer>
+
+      <PCContainer>
+        <div>
+          <H1>Mobile Controll</H1>
+        </div>
+        <PcDescriptionPanel>
+          <Small>player ships</Small>
+          <P>
+            the ships control panel is located at the bottom of the screen ,
+            after clicking it a panel will open with empty fields for the user
+            to fill in , after receiving the correct data the user will receive
+            feedback
+          </P>
+          <Small>player history</Small>
+          <P>
+            move history is located at the bottom center of the screen , when
+            opened you can select the previous move, at the bottom of the menu
+            there there will be a button to stop the update of the selected move
+            history
+          </P>
+          <Small>player Attack</Small>
+          <P>
+            to attack the opponent, open the panel located in the bottom right
+            corner and fill in the fields accordingly , or make a click on the
+            opponent's board
+          </P>
+        </PcDescriptionPanel>
+      </PCContainer>
+    </Section>
+  );
 };
 
 export default Help;
